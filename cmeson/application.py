34bb8c0b89413e10872b384b8412969fa190b7c1
Andrey Proskurin <andreyproskurin@protonmail.com>
2021-04-24 17:58:12 +0200

    added better argument validation

--- cmeson/application.py
+++ cmeson/application.py
@@ -3,6 +3,7 @@ import subprocess
 import json
 import getopt
 import sys
+import os
 from .command import LayoutCommandOutput
 from .optionlist import LayoutOptionList
 
@@ -22,6 +23,32 @@ def pop_non_argument(trailing):
 		raise IndexError
 	return trailing.pop(0)
 
+"""
+A hacky way to find out if we should run 'meson setup' or 'meson configure'
+when configuring the project
+"""
+def is_configured_project(builddir):
+	cmd = ['meson', 'introspect', '--projectinfo', builddir]
+	return subprocess.call(
+		cmd,
+		stderr=subprocess.DEVNULL,
+		stdout=subprocess.DEVNULL) == 0
+
+"""
+dargument = -D argument
+
+Returns list of darguments ['-Da=b', '-Dc=d', ...] for build options that
+have changed and need to be reconfigured
+"""
+def get_darguments(option_list):
+	widgets = map(lambda x: x[0], option_list.build_options())
+	changed = filter(lambda x: x.changed(), widgets)
+	dargs = []
+	for widget in changed:
+		darg = '-D{}={}'.format(widget.name, widget.get_value())
+		dargs.append(darg)
+	return dargs
+
 class ApplicationError(Exception):
 	pass
 
@@ -33,7 +60,7 @@ class Application:
 
 	def __init__(self, argv):
 		self.parse_arguments(argv)
-		self.configured = self.is_configured_project()
+		self.configured = is_configured_project(self.builddir)
 
 		layout = LayoutOptionList(self.get_build_options())
 		urwid.connect_signal(layout, 'configure', self.configure_meson)
@@ -71,17 +98,20 @@ class Application:
 			self.sourcedir = pop_non_argument(self.trailing)
 		except IndexError:
 			pass
-
-	"""
-	A hacky way to find out if we should run 'meson setup' or 'meson configure'
-	when configuring the project
-	"""
-	def is_configured_project(self):
-		cmd = ['meson', 'introspect', '--projectinfo', self.builddir]
-		return subprocess.call(
-			cmd,
-			stderr=subprocess.DEVNULL,
-			stdout=subprocess.DEVNULL) == 0
+		else:
+			self.get_meson_build_path()
+		
+		try:
+			if os.path.samefile(self.builddir, self.sourcedir):
+				raise ApplicationError('builddir equals sourcedir')
+		except FileNotFoundError:
+			pass
+	
+	def get_meson_build_path(self):
+		path = os.path.join(self.sourcedir, 'meson.build')
+		if not os.path.isfile(path):
+			raise ApplicationError('sourcedir not valid')
+		return path
 
 	def get_build_options(self):
 		cmd = ['meson', 'introspect', '--buildoptions']
@@ -90,7 +120,7 @@ class Application:
 		if self.configured:
 			cmd.append(self.builddir)
 		else:
-			cmd.append(self.sourcedir + '/meson.build')
+			cmd.append(self.get_meson_build_path())
 
 		try:
 			json_str = subprocess.check_output(
@@ -105,28 +135,12 @@ class Application:
 
 		return json.loads(json_str)
 	
-	"""
-	dargument = -D argument
-
-	Returns list of darguments ['-Da=b', '-Dc=d', ...] for build options that
-	have changed and need to be reconfigured
-	"""
-	@staticmethod
-	def get_darguments(option_list):
-		widgets = map(lambda x: x[0], option_list.build_options())
-		changed = filter(lambda x: x.changed(), widgets)
-		dargs = []
-		for widget in changed:
-			darg = '-D{}={}'.format(widget.name, widget.get_value())
-			dargs.append(darg)
-		return dargs
-	
 	def configure_meson(self, option_list):
 		cmd = ['meson', 'configure' if self.configured else 'setup']
 		cmd += self.trailing
 		if self.backend:
 			cmd.extend(['--backend', self.backend])
-		cmd += self.get_darguments(option_list)
+		cmd += get_darguments(option_list)
 		cmd.append(self.builddir)
 		if not self.configured:
 			cmd.append(self.sourcedir)
